b01c514242d272b049a0b4f5bd15280a3814d18f
AlmogGabay <almognv@gmail.com>
2026-04-13 20:48:56 +0300

    Add project files.

---
+++ C.A.R.E app/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace C.A.R.E_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblStatus = new Label();
+            picCamera = new PictureBox();
+            btnReset = new Button();
+            ((System.ComponentModel.ISupportInitialize)picCamera).BeginInit();
+            SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Font = new Font("Britannic Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblStatus.Location = new Point(228, 309);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(321, 31);
+            lblStatus.TabIndex = 0;
+            lblStatus.Text = "C.A.R.E - waiting for data";
+            // 
+            // picCamera
+            // 
+            picCamera.Location = new Point(242, 114);
+            picCamera.Name = "picCamera";
+            picCamera.Size = new Size(288, 192);
+            picCamera.SizeMode = PictureBoxSizeMode.Zoom;
+            picCamera.TabIndex = 1;
+            picCamera.TabStop = false;
+            // 
+            // btnReset
+            // 
+            btnReset.Font = new Font("Aharoni", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnReset.Location = new Point(332, 368);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(108, 40);
+            btnReset.TabIndex = 2;
+            btnReset.Text = "OK";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnReset);
+            Controls.Add(picCamera);
+            Controls.Add(lblStatus);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)picCamera).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblStatus;
+        private PictureBox picCamera;
+        private Button btnReset;
+    }
+}
---
+++ C.A.R.E app/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System.Threading.Tasks;
+using System;   
+namespace C.A.R.E_app
+{
+    public partial class Form1 : Form
+    {
+        FirebaseClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnReset_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The system has been successfully reset.");
+
+            this.BackColor = SystemColors.Control;
+            lblStatus.Text = "System Standby - Monitoring...";
+            lblStatus.ForeColor = Color.Black;
+
+            picCamera.Image = null;
+
+            try
+            {
+                await client.Child("AlertSystem").Child("isAlert").PutAsync(false);
+
+                MessageBox.Show("Alert acknowledged. History log preserved.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating cloud: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FirebaseClient("https://care-c0bdb-default-rtdb.europe-west1.firebasedatabase.app/");
+            
+            client.Child("AlertSystem")
+          .AsObservable<dynamic>()
+          .Subscribe(d => HandleAlertUpdate(d));
+        }
+
+        private void HandleAlertUpdate(dynamic data)
+        {
+            Console.WriteLine("Update received! Key: " + data.Key + " Value: " + data.Object);
+
+            if (data != null && data.Object != null)
+            {
+                if (data.Key == "isAlert")
+                {
+                    if ((bool)data.Object == true)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            this.BackColor = Color.DarkRed;
+                            lblStatus.Text = "Danger! Presence Detected!";
+                            lblStatus.ForeColor = Color.White;
+                        });
+                    }
+                }
+                else if (data.Key == "imageUrl")
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        string url = data.Object.ToString();
+                        picCamera.LoadAsync(url);
+                    });
+                }
+            }
+        }
+    }
+}

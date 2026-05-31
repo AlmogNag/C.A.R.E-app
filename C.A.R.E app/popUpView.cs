1a4e96810e4be04b86bce09646b3994ee780aa10
LINOYKR <linoykr@gmail.com>
2026-05-31 18:55:36 +0300

    Connecting local project and pushing updates

---
+++ C.A.R.E app/popUpView.cs
@@ -0,0 +1,79 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System.Threading.Tasks;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace C.A.R.E_app
+{
+    public partial class popUpView : Form
+    {
+        FirebaseClient client;
+
+        public popUpView()
+        {
+            InitializeComponent();
+            client = FirebaseService.Client;
+        }
+
+        private async void btnReset_Click(object sender, EventArgs e)
+        {
+            picCamera.Image = null;
+            try
+            {
+                await client.Child("AlertSystem").Child("isAlert").PutAsync(false);
+
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating cloud: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client.Child("AlertSystem")
+                  .AsObservable<dynamic>()
+                  .Subscribe(d => HandleAlertUpdate(d));
+
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void HandleAlertUpdate(dynamic data)
+        {
+            if (data != null && data.Object != null)
+            {
+                if (data.Key == "isAlert")
+                {
+                    if ((bool)data.Object == true)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            this.WindowState = FormWindowState.Normal;
+                            this.ShowInTaskbar = true;
+                            this.TopMost = true;
+
+                            this.BackColor = Color.DarkRed;
+                            lblStatus.Text = "Danger! \nPresence detected!";
+                            lblStatus.ForeColor = Color.White;
+
+                            lblStatus.Left = (this.ClientSize.Width - lblStatus.Width) / 2;
+                        });
+                    }
+                }
+                else if (data.Key == "imageUrl")
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        string url = data.Object.ToString();
+                        picCamera.LoadAsync(url);
+                    });
+                }
+            }
+        }
+    }
+}
